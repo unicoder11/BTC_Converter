fd60919c1a3dc5bb500647ecf17b325034401eb7
Gastón <elsolenllamas@gmail.com>
2017-11-13 23:41:09 -0300

    add gulp and template improvements

---
+++ Gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var sassSource = 'styles/*.scss',
+    cssDist = 'public/styles/css/';
+
+gulp.task('styles', function() {
+    gulp.src(sassSource)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(cssDist));
+});
+
+gulp.task('watch',function() {
+    gulp.watch(sassSource,['styles']);
+});
